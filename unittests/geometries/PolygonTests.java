797470c5efcd7990f839e269724c1d66a3c3e7f6
Tovat <tovatretiak@gamil.com>
2025-04-20 13:19:47 +0300

    Fixing48

--- unittests/geometries/PolygonTests.java
+++ unittests/geometries/PolygonTests.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import primitives.*;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
-//update
+
 /**
  * Testing Polygons
  * @author Dan
